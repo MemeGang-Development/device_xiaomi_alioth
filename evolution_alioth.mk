e09b9fce6250a57797e72e384fde8afa218b7c16
DarkAngelGR <asavvo01@gmail.com>
2022-09-21 05:59:17 +0530

    alioth: Evolution X Tiramisu Bringup
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- evolution_alioth.mk
+++ evolution_alioth.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2021-2022 The LineageOS Project
-# Copyright (C) 2022 VoidUI Project
+# Copyright (C) 2022 The Evolution X Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,35 +9,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common VoidUI stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-# Inherit Google Pixel Updatable APEX
-TARGET_SHIP_PREBUILT_APEX := true
-$(call inherit-product-if-exists, vendor/pixel-additional/config.mk)
-
-# Inherit Google Pixel Carrier Settings
-TARGET_INCLUDE_CARRIER_SETTINGS := true
-
-# Device Specific Flags
+# Evolution X Specific Flags
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1440
-
-# VoidUI Environment Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_STOCK_ARCORE := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_SUPPORTS_QUICK_TAP  := true
-TARGET_USES_MINI_GAPPS := false
-
-# VoidUI Maintainer Flags
-VOID_MAINTAINER := johnmart19
-CUSTOM_BUILD_TYPE := OFFICIAL
+EVO_SIGNED := true
+EVO_BUILD_TYPE := OFFICIAL
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := aosp_alioth
+PRODUCT_NAME := evolution_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
